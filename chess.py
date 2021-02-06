12f19d0d322fc44af6b7a2c21465cafce45ee900
Ineffable-sun-xie <cuexiexie@gmail.com>
2021-02-06 16:51:57 +0800

    update chess.py

--- chess.py
+++ chess.py
@@ -1,79 +1,52 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split,cross_val_score
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix,roc_curve,auc
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+# pandas处理数据集
+# sklearn.model_selection实现数据集分割，自动调参
+# sklearn.svm线性支持向量分类
+
 # 读取数据
-data = pd.read_csv('krkopt.DATA',header=None)
-data.dropna(inplace=True) # 删除缺失数据
-# 将样本数值化
-for i in [0,2,4]:
-    data.loc[data[i]=='a',i] = 1
-    data.loc[data[i]=='b',i] = 2
-    data.loc[data[i]=='c',i] = 3
-    data.loc[data[i]=='d',i] = 4
-    data.loc[data[i]=='e',i] = 5
-    data.loc[data[i]=='f',i] = 6
-    data.loc[data[i]=='g',i] = 7
-    data.loc[data[i]=='h',i] = 8
-# 将标签数值化
-data.loc[data[6]!='draw',6] = -1    
-data.loc[data[6]=='draw',6] = 1
-# 归一化处理
+data = pd.read_csv('krkopt.data', header=None)
+data.dropna(inplace=True)  # 不创建新的对象，直接对原始对象进行修改
+
+
+def svm_c(x_train, x_test, y_train, y_test):
+    # rbf核函数，设置数据权重
+    svc = SVC(kernel='rbf', class_weight='balanced', )
+    c_range = np.logspace(-5, 15, 11, base=2)
+    gamma_range = np.logspace(-9, 3, 13, base=2)
+    # 网格搜索交叉验证的参数范围，cv=3,3折交叉
+    param_grid = [{'kernel': ['rbf'], 'C': c_range, 'gamma': gamma_range}]
+    grid = GridSearchCV(svc, param_grid, cv=3, n_jobs=-1)
+    # 训练模型
+    clf = grid.fit(x_train, y_train)
+    # 计算测试集精度
+    score = grid.score(x_test, y_test)
+    print('精度为%s' % score)
+
+
+# 样本数值化 a,b,c...h 转化为 1,2,3...8
+for i in [0, 2, 4]:
+    data.loc[data[i] == 'a', i] = 1
+    data.loc[data[i] == 'b', i] = 2
+    data.loc[data[i] == 'c', i] = 3
+    data.loc[data[i] == 'd', i] = 4
+    data.loc[data[i] == 'e', i] = 5
+    data.loc[data[i] == 'f', i] = 6
+    data.loc[data[i] == 'g', i] = 7
+    data.loc[data[i] == 'h', i] = 8
+
+# 将标签数值化 -1表示将死，1表示和棋
+data.loc[data[6] != 'draw', 6] = -1
+data.loc[data[6] == 'draw', 6] = 1
+
+# 归一化处理 Z-score标准化方法 （经过处理的数据符合标准正态分布，即均值为0，标准差为1）
 for i in range(6):
-    data[i] = (data[i]-data[i].mean())/data[i].std()  
-# 拆分训练集和测试集
-X_train, X_test, y_train, y_test =  train_test_split(data.iloc[:, :6], data[6], test_size=0.82178500142572)
-print(y_train.dtype)
-# 寻找C和gamma的粗略范围
-CScale = [i for i in range(100,201,10)];
-gammaScale = [i/10 for i in range(1,11)];
-cv_scores = 0
-for i in CScale:
-    for j in gammaScale:
-        model = SVC(kernel = 'rbf', C = i,gamma=j)
-        scores = cross_val_score(model,X_train, y_train,cv =5,scoring = 'accuracy')
-        if scores.mean()>cv_scores:
-            cv_scores = scores.mean()
-            savei = i
-            savej = j*100
-# 找到更精确的C和gamma
-CScale = [i for i in range(savei-5,savei+5)];
-gammaScale = [i/100+0.01 for i in range(int(savej)-5,int(savej)+5)];
-cv_scores = 0
-for i in CScale:
-    for j in gammaScale:
-        model = SVC(kernel = 'rbf', C = i,gamma=j)
-        scores = cross_val_score(model,X_train, y_train,cv =5,scoring = 'accuracy')
-        if scores.mean()>cv_scores:
-            cv_scores = scores.mean()
-            savei = i
-            savej = j
-#将确定好的参数重新建立svm模型
-model = SVC(kernel = 'rbf', C=savei,gamma=savej)
-model.fit(X_train, y_train)
-pre = model.predict(X_test)
-model.score(X_test,y_test)
-# 绘制AUC和EER图形
-cm = confusion_matrix(y_test, pre, labels=[-1, 1], sample_weight=None)
-sns.set()
-f,ax=plt.subplots()
-sns.heatmap(cm,annot=True,ax=ax) #画热力图
-ax.set_title('confusion matrix') #标题
-ax.set_xlabel('predict') #x轴
-ax.set_ylabel('true') #y轴
-fpr,tpr,threshold = roc_curve(y_test, pre) ###计算真正率和假正率
-roc_auc = auc(fpr,tpr) ###计算auc的值，auc就是曲线包围的面积，越大越好
-plt.figure()
-lw = 2
-plt.figure(figsize=(10,10))
-plt.plot(fpr, tpr, color='darkorange',lw=lw, label='ROC curve (area = %0.2f)' % roc_auc) ###假正率为横坐标，真正率为纵坐标做曲线
-plt.plot([0, 1], [1, 0], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
-plt.legend(loc="lower right")
-plt.show()
+    data[i] = (data[i] - data[i].mean()) / data[i].std()
+
+# 拆分训练集和测试集    train_teat_split() https://www.cnblogs.com/bonelee/p/8036024.html 省略random_state=0
+X_train, X_test, y_train, y_test = train_test_split(data.iloc[:, :6], data[6].astype(int), test_size=0.82178500142572)
+svm_c(X_train, X_test, y_train, y_test)
